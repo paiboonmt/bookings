e32edc1d067e25ac33450d8f76b295dc8ee5996d
paiboonmt <paiboon500@gmail.com>
2025-02-20 15:59:37 +0700

    update

--- customerRegister.php
+++ customerRegister.php
@@ -1,10 +1,12 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | Registration Page (v2)</title>
+  <title>Booking | Register</title>
+  <!-- Icon -->
+  <link rel="icon" type="image/x-icon" href="/images/logo.png">
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -14,64 +16,78 @@
   <link rel="stylesheet" href="public/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
   <!-- Theme style -->
   <link rel="stylesheet" href="public/dist/css/adminlte.min.css">
+  <style>
+        body {
+            background-image: url('images/bg.jpg');
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-size: cover;
+        }
+        #login-logo {
+            background-color: #f8f9fa;
+            border-radius: 10px;
+        }
+    </style>
 </head>
+
 <body class="hold-transition register-page">
-<div class="register-box">
-  <div class="card card-outline card-primary">
-    <div class="card-header text-center">
-      <a href="../../index2.html" class="h1"><b>Booking</b>Member</a>
-    </div>
-    <div class="card-body">
-      <p class="login-box-msg">Register a new membership</p>
+  <div class="register-box">
+    <div class="card card-outline card-primary">
+      <div class="card-header text-center">
+        <a href="../../index2.html" class="h1"><b>Booking</b>Member</a>
+      </div>
+      <div class="card-body">
+        <p class="login-box-msg">Register a new membership</p>
 
-      <form action="" method="post">
-        <div class="input-group mb-3">
-          <input type="text" class="form-control" name="name" required placeholder="Full name">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-user"></span>
+        <form action="" method="post">
+          <div class="input-group mb-3">
+            <input type="text" class="form-control" name="name" required placeholder="Full name">
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-user"></span>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="input-group mb-3">
-          <input type="email" class="form-control" name="email" required placeholder="Email">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-envelope"></span>
+          <div class="input-group mb-3">
+            <input type="email" class="form-control" name="email" required placeholder="Email">
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-envelope"></span>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="input-group mb-3">
-          <input type="number" class="form-control" name="phone" required placeholder="Phone">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-phone"></span>
+          <div class="input-group mb-3">
+            <input type="number" class="form-control" name="phone" required placeholder="Phone">
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-phone"></span>
+              </div>
             </div>
           </div>
-        </div>
-    
-        <div class="row">
-          
-          <!-- /.col -->
-          <div class="col">
-            <button type="submit" class="btn btn-primary btn-block">Register</button>
+
+          <div class="row">
+
+            <!-- /.col -->
+            <div class="col">
+              <button type="submit" class="btn btn-primary btn-block">Register</button>
+            </div>
+            <!-- /.col -->
           </div>
-          <!-- /.col -->
-        </div>
-      </form>
+        </form>
 
-    </div>
-    <!-- /.form-box -->
-  </div><!-- /.card -->
-</div>
-<!-- /.register-box -->
+      </div>
+      <!-- /.form-box -->
+    </div><!-- /.card -->
+  </div>
+  <!-- /.register-box -->
 
-<!-- jQuery -->
-<script src="public/plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="public/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
-<script src="public/dist/js/adminlte.min.js"></script>
+  <!-- jQuery -->
+  <script src="public/plugins/jquery/jquery.min.js"></script>
+  <!-- Bootstrap 4 -->
+  <script src="public/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- AdminLTE App -->
+  <script src="public/dist/js/adminlte.min.js"></script>
 
 </body>
+
 </html>
